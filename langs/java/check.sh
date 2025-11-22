c1db4202d54ebda3c62cbf5d961dee05e6ecba6d
Matheus Araujo <matheus.saraujo@gmail.com>
2025-11-22 14:52:58 +0100

    feat: check command, java

--- langs/java/check.sh
+++ langs/java/check.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-source lib/aoc/utils.sh
+source $ROOT/core/_utils.sh
 
-year=$1
-day=$2
+dir=$1
+year=$2
+day=$3
 
-files=("part1.java" "part2.java" "helpers.java")
+files=("part1.java" "part2.java" "part3.java" "helpers.java")
 
 for file in "${files[@]}"; do
-    if [ -f "$year/day$day/$file" ]; then
-        pmd check -d $year/day$day/$file -R lib/java/ruleset.xml --no-cache
+    if [ -f "$dir/$file" ]; then
+        output=$(pmd check --no-progress -d "$dir/$file" -R "$ROOT/langs/java/ruleset.xml" --no-cache 2>&1)
         if [ $? -ne 0 ]; then
+            print_line "${PURPLE}pmd check${GRAY_ITALIC} $dir/$file ${CHECK_ERROR}"
+            print_line "$output"
             exit 1
         else
-            print_line "pmd check $year/day$day/$file ${CHECK_SUCCESS}"
+            print_line "${PURPLE}pmd check${GRAY_ITALIC} $dir/$file ${CHECK_SUCCESS}"
         fi
     fi
 done
